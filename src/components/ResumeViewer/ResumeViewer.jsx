566f22e0add4e3407572cd01e9d3b2d68b257693
Huu Phuc Nguyen <nhphuc236@gmail.com>
2025-05-24 23:41:25 -0500

    chore: update resume

--- src/components/ResumeViewer/ResumeViewer.jsx
+++ src/components/ResumeViewer/ResumeViewer.jsx
@@ -22,7 +22,7 @@ const ResumeViewer = ({isShow, setShowResume}) => {
     <section className="fixed inset-0 flex items-center justify-center w-full h-full bg-black/80 z-50" onClick={handleOnClick}>
       <div className='fixed m-4 top-0 flex flex-row w-full justify-end px-10 md:px-60 md:top-4 gap-4'>
         <a
-          href="https://drive.google.com/uc?id=1eSi65Go6jwQptNQv8QJzd9_YnRPwEZtU&export=download"
+          href="https://drive.google.com/uc?id=1Ge4DyzdCSLwytvcjjZ9NPkM02oSheuHr&export=download"
           download
           className= "text-white"
         >
@@ -35,7 +35,7 @@ const ResumeViewer = ({isShow, setShowResume}) => {
       </div>
       
       <iframe
-              src="https://drive.google.com/file/d/1eSi65Go6jwQptNQv8QJzd9_YnRPwEZtU/preview"
+              src="https://drive.google.com/file/d/1Ge4DyzdCSLwytvcjjZ9NPkM02oSheuHr/preview"
               title="Resume"
               className="border-0 rounded-lg w-full md:w-1/2 md:h-full h-[80%] py-4"
       />
